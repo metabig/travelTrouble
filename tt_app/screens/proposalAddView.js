20fc44fee04edc7aac5d3159337640ac82d53144
metabig <tonibergas.tb@gmail.com>
2019-10-13 01:35:15 +0200

    Deleting Bug

--- tt_app/screens/proposalAddView.js
+++ tt_app/screens/proposalAddView.js
@@ -2,38 +2,6 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 import { Input } from 'react-native-elements';
 
-export default class ProposalAddScreen extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <View style={styles.basic}>
-        <Text>Jan's View (Add)</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  basic: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  }
-});
-
-
-
-
-
-
-
-import React, { Component } from 'react';
-import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
-import { Input } from 'react-native-elements';
-
 export default class FlightInfoScreen extends React.Component {
   constructor(props) {
     super(props);
