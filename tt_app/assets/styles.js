f658f40bfc89efc054a2d4eaa1f5b0bbc52466a5
metabig <tonibergas.tb@gmail.com>
2019-10-13 06:08:42 +0200

    styles

---
+++ tt_app/assets/styles.js
@@ -0,0 +1,37 @@
+import { StyleSheet } from 'react-native';
+
+
+export default styles = StyleSheet.create({
+    basic: {
+        flex: 1,
+        backgroundColor: '#f7f7f7',
+        padding: 30
+    },
+    bigText:
+    {
+        fontWeight: 'bold',
+        fontSize: 30,
+        padding: 20
+
+    },
+    normalText:
+    {
+        fontSize: 20,
+        padding:20
+    },
+    padding: {
+        padding: 10
+    },
+    border: 
+    {
+        borderWidth: 1
+    },
+    proposal: {
+        backgroundColor: '#fff',
+        marginBottom: 10,
+
+    },
+    margin: {
+        marginBottom:40
+    }
+});
